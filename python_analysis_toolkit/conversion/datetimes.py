1f9e276b6847129c671815eb06b54efb31bd0eb9
Tommy Carpenter <tommyjcarpenter@gmail.com>
2015-09-08 12:51:42 -0400

    new functionality

--- python_analysis_toolkit/conversion/datetimes.py
+++ python_analysis_toolkit/conversion/datetimes.py
@@ -1,11 +1,13 @@
 import datetime
 import time
 
+def ymdhms_to_epoch(input_str):
+    return int(time.mktime(ymdhms_to_datetime(time.strptime(input_str, '%Y-%m-%d %H:%M:%S'))))
+
 def ymdhms_to_datetime(input_str, truncate_time = False):
     d = datetime.datetime.fromtimestamp(time.mktime(time.strptime(input_str,'%Y-%m-%d %H:%M:%S')))
     return d.replace(second=0, minute=0, hour=0) if truncate_time else d
     
-def ymdhms_to_epoch(input_str):
-    return int(time.mktime(ymdhms_to_datetime(time.strptime(input_str, '%Y-%m-%d %H:%M:%S'))))
-
-
+def epoch_to_datetime(input_epoch, truncate_time = False):
+    d = datetime.datetime.utcfromtimestamp(input_epoch)
+    return d.replace(second=0, minute=0, hour=0) if truncate_time else d
