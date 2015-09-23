301a9c1bc67042108400759c9f7511a4b5ad149a
Tommy Carpenter <tommy@research.att.com>
2015-09-23 13:42:21 -0400

    New functionality.

--- python_analysis_toolkit/stats/basic_functions.py
+++ python_analysis_toolkit/stats/basic_functions.py
@@ -1,5 +1,7 @@
 import operator
 from collections import Counter
+import numpy
+from math import sqrt   
 
 def list_to_frequency_tuples(l):
     """
@@ -17,3 +19,13 @@ def list_to_frequency_tuples(l):
     for key, value in sorted(c.items(), key=operator.itemgetter(1), reverse=True):
         return_tups.append((key, value, 100*value/N))
     return return_tups
+
+def mean(l):
+    return numpy.mean(l)
+
+def ci95(l):
+    """
+        Returns the 95% Gaussian confidence interval (caller should use the + and - of this value) of an array of vals
+        http://en.wikipedia.org/wiki/Confidence_interval
+    """
+    return 1.96*numpy.std(l)/sqrt(len(l)) 
