c0c71c28f96e7d967a852d9e135c64d1cf532bdd
Tommy Carpenter <tommy@research.att.com$>
2015-10-22 17:18:50 -0400

    Enhancements to PCA function to better show labels, and allow for a mean normalization.

--- python_analysis_toolkit/machine_learning/dimensionality.py
+++ python_analysis_toolkit/machine_learning/dimensionality.py
@@ -10,33 +10,43 @@ from python_analysis_toolkit.machine_learning import clustering
 
 _colors = ['r', 'b', 'g', 'k', 'm', 'c', 'y']  
 
-def pca_biplot_with_clustering(data_matrix, feature_labels, K = 5, n_components=2, f_out = "foo"):
+def pca_biplot_with_clustering(data_matrix, feature_labels, mean_normalize = False, K = 5, n_components=2, f_out = "foo"):
     """
     Inputs:
-        data_matrix- numpy array of shape n x f where n is the number of samples and f is the number of features (variables)
-        feature_labels - the list of human-interpretable labels for the rows in data_matrix. Used for the biplot
+        data_matrix: numpy array of shape n x f where n is the number of samples and f is the number of features (variables)
+        feature_labels: the list of human-interpretable labels for the rows in data_matrix. Used for the biplot
+        mean_normalize: if true, each row i of data_matrix is replaced with i - mean(i)
         n_components: the number of PCA vectors you want. 
         f_out: the path to write the graph (as PDF) to
         
-        NOTE: the graph currently only supports n=2. 
+        TODO: the graph currently only supports n=2. 
     
     Outputs:
-        None, but writes to a file on disk
+        r_loadings: the loading vectors
+        
+    Side Effects:
+        file created on disk at f_out
     
-        References:
+    References:
         http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
         https://github.com/teddyroland/python-biplot/blob/master/biplot.py
         http://stackoverflow.com/questions/21217710/factor-loadings-using-sklearn
         http://stackoverflow.com/questions/14716965/r-principle-component-analysis-label-of-component
         http://nxn.se/post/36838219245/loadings-with-scikit-learn-pca
     """    
-    
     data_matrix = np.nan_to_num(data_matrix) #clustering does not allow infs, nans
+        
+    if mean_normalize:   
+        for iindex, i in enumerate(data_matrix):
+            data_matrix[iindex] = i - np.mean(i)
+
     rows, variables = np.shape(data_matrix)
     pca = PCA(n_components=n_components)
-    transformed_matrix = pca.fit_transform(data_matrix)
+    pca.fit(data_matrix)
     
     r_loadings = np.matrix.transpose(pca.components_) #components is n_components x f; transpase to get rows as features, like R does it 
+    
+    transformed_matrix = fast_dot(data_matrix, r_loadings)
     
     loading_vectors = []
     loading_labels = []
@@ -61,20 +71,22 @@ def pca_biplot_with_clustering(data_matrix, feature_labels, K = 5, n_components=
     pxs = [i[0] for i in loading_vectors]
     pys =  [i[1] for i in loading_vectors]    
     for xindex, x in enumerate(pxs):     
-        ax.arrow(0, 0, pxs[xindex], pys[xindex], width=0.0005, head_width=0.0025, color="r")
-        ax.text(pxs[xindex], pys[xindex], loading_labels[xindex], color="r")
+        ax.arrow(0, 0, pxs[xindex], pys[xindex], linewidth=3, width=0.0005, head_width=0.0025, color="r", label = "loading")
+        ax.arrow(0, 0, pxs[xindex]*5, pys[xindex]*5, alpha = .5, linewidth = 1, linestyle="dashed", width=0.0005, head_width=0.0025, color="r")
+        ax.text(pxs[xindex]*5, pys[xindex]*5, loading_labels[xindex], color="r")
     
+    ax.legend(loc='best')
     x0,x1 = ax.get_xlim()
     y0,y1 = ax.get_ylim()
-    ax.set_xlim(min(x0, y0), max(x1,y1))
+    ax.set_xlim(min(x0, y0), max(x1,y1)) #make it square
     ax.set_ylim(min(x0, y0), max(x1,y1))      
     ax.set_xlabel("PCA[0]")
     ax.set_ylabel("PCA[1]") 
     ax.set_title("", fontsize = 20)
     ax.grid(b=True, which='major', color='k', linestyle='--')
 
-    fig.savefig(f_out + "{0}".format(K) + ".pdf", format="pdf")    
+    fig.savefig(f_out + "{0}{1}".format(K, "_normalized" if mean_normalize else "") + ".pdf", format="pdf")    
     plt.close() #THIS IS CRUCIAL SEE:  http://stackoverflow.com/questions/26132693/matplotlib-saving-state-between-different-uses-of-io-bytesio
-    
+    return r_loadings
 
 
